935d602f4b604bdfe8cc3b7a00bfb840515bc1b3
Andrew McInally <realfunnyestate@yahoo.com>
2022-04-03 23:41:41 -0600

    mas y mas

--- index.js
+++ index.js
@@ -1,7 +1,88 @@
-// TODO: Include packages needed for this application
+//packages needed for this application
+const inquirer = require("inquirer");
+const genterateMarkdown = require("./utils/generateMarkdown");
+const fs = require("fs");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// an array of questions for user input
+const questions = [
+  //project title
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+    validate: (titleInput) => {
+      if (titleInput) {
+        return true;
+      } else {
+        console.log("You MUST enter a project title");
+        return false;
+      }
+    },
+  },
+  //description of project
+  {
+    type: "input",
+    name: "description",
+    message: "Type a short description of your project",
+    validate: (descriptionInput) => {
+      if (descriptionInput) {
+        return true;
+      } else {
+        console.log("You MUST enter a description");
+        return false;
+      }
+    },
+  },
+  //installation of project
+  {
+    type: "input",
+    name: "installation",
+    message:
+      "Type a short explanation of your installation process of the project",
+    validate: (installationInput) => {
+      if (installationInput) {
+        return true;
+      } else {
+        console.log("You MUST enter an installation configuration");
+        return false;
+      }
+    },
+  },
+  //usage for project
+  {
+    type: "input",
+    name: "usage",
+    message: "What is the project used for?",
+    validate: (usageInput) => {
+      if (usageInput) {
+        return true;
+      } else {
+        console.log("You MUST enter a use case for the project");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "contribution",
+    message: "How can developers contribute to this project?",
+    validate: contributionInput => {
+      if (contributionInput) {return true;
+    }else{
+      console.log('You MUST enter a contribution description for the project');
+      return false;
+    }
+  },
+  {
+    type: "input",
+    name: 'testing',
+    message: 'How do you test this project'
+  },
+  {
+    type: "checkbox",
+    name: "licensing",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,7 +13,42 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  // ![Github license](http://img.shields.io/badge/license-${data.license}-blue.svg)
+  [![license](https://img.shields.io/badge/license-${data.licensing}-blue)](https://shields.io)
 
+  ## Description 
+  ${data.description}
+  ## Table of Contents
+
+  * [Description](#Description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contribution](#contribution)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Description
+  ${data.description}
+
+  ## Installation 
+  ${data.install}
+
+  ## Usage 
+  ${data.usage}
+
+  ## Contributing 
+  ${data.contribution}
+
+  ## Tests
+  ${data.test}
+
+  ## License 
+  This project is license under ${data.license}
+
+  ## Questions
+  - Github: [${data.github}](https://github.com/${data.github})
+  - Email: ${data.email} 
 `;
 }
 
